db4471bbb1e994c94a721f638ec6f9b1e8732a90
Jeremias <jerexxypunto@gmail.com>
2022-06-18 14:32:32 -0400

    agrego selectores y corrijo error un funcion hassClass

--- gscjs.js
+++ gscjs.js
@@ -1,32 +1,53 @@
 const GSCJS = {
-    autor:"Jeremías San Martín",
-    web:"https://gscdesigns.net/",
-    /**
-     * Esta funcion retorna un nodo HTML con su contenido.
-     * 
-     * @param {string} tag Una etiqueta HTML
-     * @param {string} txt El contenido de la etiqueta HTML
-     * @returns {NodeHTML} Un nodo
-     */
-    crearNodo : function crearNodo(tag,txt){
-      const node = document.createElement(tag);
-      node.textContent = txt;
-      return node;
-    },
-    /**
-     * Esta funcion retorna true si el nodo objetivo tiene la classe
-     * espesificada y false si no tiene la clase.
-     * 
-     * @param {NodeHTML} target El NodoHTML objeto de la verificación
-     * @param {string} classname 
-     * @returns {boolean}
-     */
-    hasClass: function hasClass(target,classname){
-        const target = document.querySelector(target);
-        const response = target.classList.contain(classname);
-        return response;
-    }
-}
+  autor: "Jeremías San Martín",
+  web: "https://gscdesigns.net/",
+  /**
+   * Selecciona un solo elemento en le documeneto
+   * @param {nodeHTML} element
+   * @returns {NodeHTML}
+   */
+  queryOnly: function queryOnly(element) {
+    const selection = document.querySelector(element);
+    return selection;
+  },
+  /**
+   * Toma un nodeList del DOM y retorna un
+   * Array con todos sus elementos.
+   * 
+   * @param {NodeList} element
+   * @returns {Array}
+   */
+  queryAll: function queryAll(element) {
+    const nodeList = document.querySelectorAll(element);
+    const selection = [...nodeList];
+    return selection;
+  },
+  /**
+   * Esta funcion retorna un nodo HTML con su contenido.
+   *
+   * @param {string} tag Una etiqueta HTML
+   * @param {string} txt El contenido de la etiqueta HTML
+   * @returns {NodeHTML} Un nodo
+   */
+  crearNodo: function crearNodo(tag, txt) {
+    const node = document.createElement(tag);
+    node.textContent = txt;
+    return node;
+  },
+  /**
+   * Esta funcion retorna true si el nodo objetivo tiene la classe
+   * espesificada y false si no tiene la clase.
+   *
+   * @param {NodeHTML} targetObjet El NodoHTML objeto de la verificación
+   * @param {string} classname
+   * @returns {boolean}
+   */
+  hasClass: function hasClass(target, classname) {
+    const targetObjet = document.querySelector(target);
+    const response = targetObjet.classList.contain(classname);
+    return response;
+  },
+};
 
 
   
