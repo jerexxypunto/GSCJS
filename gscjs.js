ae8447df0c25a6017ba8b85c1614455980d4f268
jerexxypunto_gear <jerexxypunto@gmail.com>
2023-04-06 23:41:36 -0400

    convierto a objeto

--- gscjs.js
+++ gscjs.js
@@ -1,13 +1,17 @@
-const GSCJS = {
-  /**
+class GSCJS {
+  constructor() { 
+
+   }
+   
+    /**
    * Selecciona un solo elemento en le documeneto
    * @param {nodeHTML} element
    * @returns {NodeHTML}
    */
-  queryOnly: function queryOnly(element) {
+   queryOnly(element) {
     const selection = document.querySelector(element);
     return selection;
-  },
+  }
   /**
    * Toma un nodeList del DOM y retorna un
    * Array con todos sus elementos.
@@ -15,11 +19,11 @@ const GSCJS = {
    * @param {NodeList} element
    * @returns {Array}
    */
-  queryAll: function queryAll(element) {
+  queryAll(element) {
     const nodeList = document.querySelectorAll(element);
     const selection = [...nodeList];
     return selection;
-  },
+  }
   /**
    * Esta funcion retorna un nodo HTML con su contenido.
    *
@@ -27,11 +31,11 @@ const GSCJS = {
    * @param {string} txt El contenido de la etiqueta HTML
    * @returns {NodeHTML} Un nodo
    */
-  crearNodo: function crearNodo(tag, txt) {
+  crearNodo(tag, txt) {
     const node = document.createElement(tag);
     node.textContent = txt;
     return node;
-  },
+  }
   /**
    * Esta funcion retorna true si el nodo objetivo tiene la classe
    * espesificada y false si no tiene la clase.
@@ -40,51 +44,51 @@ const GSCJS = {
    * @param {string} classname
    * @returns {boolean}
    */
-  hasClass: function hasClass(target, classname) {
+  hasClass(target, classname) {
     const targetObjet = document.querySelector(target);
     const response = targetObjet.classList.contain(classname);
     return response;
-  },
+  }
   // Retorna toda la URL
-  getURL: function getURL() {
+  getURL() {
     const url = location.href;
     return url;
-  },
+  }
   // Retorna solo la URL
-  getURLorigin: function getURLorigin() {
+  getURLorigin() {
     const url = location.origin;
     return url;
-  },
+  }
   // Retorna la ruta del URL (lo que le sigue a la extención midominio.com/ruta)
-  getURLpath: function getURLpath() {
+  getURLpath() {
     const url = location.pathname;
     return url;
-  },
+  }
   // Retorna el hash
-  getURLhash: function getURLhash() {
+  getURLhash() {
     let url = location.hash;
     if (url == "") {
       url = undefined;
     }
     return url;
-  },
+  }
   /**
    *
    * @returns documentfragment
    */
-  crearFragmento: function fragmento() {
+  crearFragmento() {
     const documentFragement = document.createDocumentFragment();
     return documentFragement;
-  },
-  getParameterByName: function getParameterByName(name) {
+  }
+  getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
       results = regex.exec(location.search);
     return results === null
       ? ""
       : decodeURIComponent(results[1].replace(/\+/g, " "));
-  },
-  setCounter: function setCounter(nodoTarget, time) {
+  }
+  setCounter(nodoTarget, time) {
     let finalCount = nodoTarget.getAttribute("data-gsc-count");
     if(finalCount != null){
       finalCount = parseInt(finalCount);
@@ -100,8 +104,10 @@ const GSCJS = {
       console.error(`No has definido la propiedad 'data-gsc-count'`);  
     }
     
-  },
-};
+  }
+}
+
+
 
 async function gscFetch(url, callback, mode) {
   const get = await fetch(url);
